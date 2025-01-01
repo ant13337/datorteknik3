19dca372f93fba4bea0ea5a39e828f9eccbb444f
anton <anton.m.lindstrom@gmail.com>
2025-01-01 14:44:26 +0100

    ok

---
+++ lab3-files-to-students/lab3-files-to-students/lab3_lib.s
@@ -0,0 +1,12 @@
+;void inImage();
+;long long getInt();
+;int getText(char *, int);
+;int getInPos();
+;void setInPos(int);
+
+;void outImage();
+;void putInt(long long);
+;void putText(char *);
+;void putChar(char);
+;int getOutPos();
+;void setOutPos(int);
